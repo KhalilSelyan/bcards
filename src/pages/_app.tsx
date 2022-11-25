a14cfd107f48612109c53e6df9861e2e5ba754d6
Khalil Selyan <khalil@leodrive.ai>
2022-11-25 22:59:19 +0300

    navbar + clean home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import Navbar from "../components/Navbar";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,6 +13,8 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Navbar />
+
       <Component {...pageProps} />
     </SessionProvider>
   );
