da71decede8b8931f47f3552dfd78cd1e4143233
Khalil Selyan <khalil@leodrive.ai>
2022-11-26 01:00:11 +0300

    fix og

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -8,11 +8,12 @@ export const config = {
 
 const og = async (req: NextApiRequest, res: NextApiResponse) => {
   // Get query params from request
-  const url = new URL(req.url!, "http://localhost:3000");
+  if (!req.url) return res.status(400).send("Bad Request");
+  const url = new URL(req.url, "https://bcards.vercel.app/");
   const username = url.searchParams.get("username");
   const title = url.searchParams.get("title");
   const imgSrc = url.searchParams.get("imgSrc");
-
+  if (!imgSrc) return res.status(400).send("Bad Request");
   if (!username) return res.status(400).json({ error: "Username is required" });
 
   return new ImageResponse(
@@ -25,7 +26,7 @@ const og = async (req: NextApiRequest, res: NextApiResponse) => {
       >
         <div tw="flex flex-row">
           <img
-            src={imgSrc!}
+            src={imgSrc}
             tw="w-24 h-24 rounded-full shadow-2xl mb-4 mr-6"
             style={{
               objectPosition: "center",
